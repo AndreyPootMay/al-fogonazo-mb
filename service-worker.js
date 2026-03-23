2ece426fcfd1e1bfd2a1d67cf76c794dc9efc904
AndreyPootMay <andreypootmay@gmail.com>
2026-03-23 12:14:45 -0500

    PWA

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'al-fogonazo-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(res => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, res.clone());
+          return res;
+        });
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
